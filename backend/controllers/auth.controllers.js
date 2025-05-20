e0d61cb97876e3cb7a63004bdaacaf7414bb5f15
HVBLEGENDZZ9 <harshvardhanb20@cse.iiitp.ac.in>
2025-05-20 12:41:24 +0530

    setting up signup api

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -1,4 +1,5 @@
 import User from '../models/user.model.js';
+import bcrypt from 'bcryptjs';
 
 export const signup = async (req,res) => {
     try{
@@ -14,25 +15,34 @@ export const signup = async (req,res) => {
             return res.status(400).json({error: "Username already exists!"});
         }
         
-        maleProfilePic = 'https://avatar.iran.liara.run/public/boy?username=${username}';
-        femaleProfilePic = 'https://avatar.iran.liara.run/public/girl?username=${username}';
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        const maleProfilePic = 'https://avatar.iran.liara.run/public/boy?username=${userName}';
+        const femaleProfilePic = 'https://avatar.iran.liara.run/public/girl?username=${userName}';
 
         const newUser = new User({
             fullName,
             userName,
-            password,
+            password: hashedPassword,
             gender,
             profilePic : gender == "male" ? maleProfilePic : femaleProfilePic
         })
 
-        await newUser.save();
+        if(newUser){
+            await newUser.save();
 
-        res.status(201).json({
-            _id : newUser._id,
-            fullName: newUser.fullName,
-            userName: newUser.userName,
-            profilePic: newUser.profilePic
-        })
+            res.status(201).json({
+                _id : newUser._id,
+                fullName: newUser.fullName,
+                userName: newUser.userName,
+                profilePic: newUser.profilePic
+            });
+        }
+        else{
+            res.status(400).json({error: "Invalid user data"});
+        }
+        
     } catch (error){
         console.log("Error in signup controller", error.message);
         res.status(500).json({error:"Internal Server Error"});
